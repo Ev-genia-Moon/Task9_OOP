db689d5ca40ea9fe34392ef0bc39d1eba1e5614c
Eva.Kusyakina <eva.kusyakina@gmail.com>
2024-10-03 16:09:57 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // поле для фамилии
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String passport; // поле для паспорта
+    public Boolean subscription; // поле для согласия на смс рассылку
+    public FormDate birthday; // поле для даты рождения
+}
